909ac56a3de9c8c32dcefb08c3ed49aa3e3e11ea
Johannes Wachter <johannes.wachter@massiveart.com>
2014-10-23 14:18:31 +0200

    added websocket package

--- packages/custom/websocket/app.js
+++ packages/custom/websocket/app.js
@@ -13,40 +13,20 @@ var Websocket = new Module('websocket');
  */
 Websocket.register(function(app, auth, database) {
 
-  //We enable routing. By default the Package Object is passed to the routes
-  Websocket.routes(app, auth, database);
+    //We enable routing. By default the Package Object is passed to the routes
+    Websocket.routes(app, auth, database);
 
-  //We are adding a link to the main menu for all authenticated users
-  Websocket.menus.add({
-    title: 'websocket example page',
-    link: 'websocket example page',
-    roles: ['authenticated'],
-    menu: 'main'
-  });
-  
-  Websocket.aggregateAsset('css', 'websocket.css');
-
-  /**
-    //Uncomment to use. Requires meanio@0.3.7 or above
-    // Save settings with callback
-    // Use this for saving data from administration pages
-    Websocket.settings({
-        'someSetting': 'some value'
-    }, function(err, settings) {
-        //you now have the settings object
+    //We are adding a link to the main menu for all authenticated users
+    Websocket.menus.add({
+        title: 'websocket example page',
+        link: 'websocket example page',
+        roles: ['authenticated'],
+        menu: 'main'
     });
 
-    // Another save settings example this time with no callback
-    // This writes over the last settings.
-    Websocket.settings({
-        'anotherSettings': 'some value'
-    });
+    Websocket.aggregateAsset('css', 'websocket.css');
 
-    // Get settings. Retrieves latest saved settigns
-    Websocket.settings(function(err, settings) {
-        //you now have the settings object
-    });
-    */
 
-  return Websocket;
+
+    return Websocket;
 });
