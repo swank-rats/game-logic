d7bc15d0d9090bc6f2886facebcdeacd8b534e7a
Michael Zangerle <michael.zangerle@gmail.com>
2014-12-26 18:43:58 +0100

    removed comments, added error message

--- packages/custom/games/public/services/util.js
+++ packages/custom/games/public/services/util.js
@@ -13,15 +13,11 @@ angular.module('mean.games').service('GamesUtil', [function() {
                 var url = '/games/'+game._id+'?action=start';
 
                 $http.post(url, {msg:'hello word!'}).
-                    success(function(data, status, headers, config) {
-                        // this callback will be called asynchronously
-                        // when the response is available
-                        console.log(data, status);
-                    }).
-                    error(function(data, status, headers, config) {
-                        // called asynchronously if an error occurs
-                        // or server returns response with an error status.
-                        console.log(data, status);
+                    error(function(data, status, headers) {
+                        console.error(
+                            'Error while starting game!',
+                            {data:data, status:status, headers:headers}
+                        );
                     });
             },
 
