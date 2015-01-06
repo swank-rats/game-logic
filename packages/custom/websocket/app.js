35bf965bda96a037bf51cb7ebb15f0e3f608c5a1
Wachter Johannes <johannes.wachter@massiveart.at>
2015-01-06 16:10:33 +0100

    implemented auth for websockets

--- packages/custom/websocket/app.js
+++ packages/custom/websocket/app.js
@@ -8,12 +8,50 @@ var ModuleFactory = require('meanio').Module;
 var Websocket = new ModuleFactory('websocket'),
     WebsocketServer = require('websocket-wrapper').WebsocketWrapper,
     websocketSever;
+
+var httpBasic = require('basic-auth');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+var Q = require('q');
+
+var sockets = {};
+
 /*
  * All MEAN packages require registration
  * Dependency injection is used to define required modules
  */
-Websocket.register(function(app, auth, database, https) {
+Websocket.register(function(app, auth, database, https, passport) {
+    https.on('upgrade', function(req, socket, head) {
+        var basicUser = httpBasic(req),
+            def = Q.defer();
+
+        req.prom = def.promise;
+
+        User.findOne({
+            email: basicUser.name
+        }, function(err, user) {
+            if (err || !user || !user.authenticate(basicUser.pass)) {
+                def.reject();
+            }else {
+                req.user = user;
+                def.resolve();
+            }
+        });
+    }.bind(this));
+
     websocketSever = new WebsocketServer({server: https});
+
+    websocketSever.on('connection', function(event, socket) {
+        socket.upgradeReq.prom.then(function() {
+            var username = socket.upgradeReq.user.get('name');
+
+            sockets[username] = socket;
+        });
+
+        socket.upgradeReq.prom.fail(function() {
+            socket.close();
+        });
+    });
 
     websocketSever.addListener('test', {echo: function(socket, params, data) {
         if (!!params.toUpper) {
